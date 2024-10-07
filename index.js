d64746a96520e6fc78cc94f72b8b55c572a28a5f
PratikZajam <zajampratik@gmail.com>
2024-10-07 13:40:13 +0530

    added index.js file into repo

---
+++ index.js
@@ -0,0 +1,15 @@
+const numbers = [
+    3, 7, 2, 15, 9, 20, 5, 14, 12, 1, 8, 11, 6, 19, 4, 10, 17, 13, 16, 18,
+  ];
+
+
+  for(let i=0;i<=numbers.length;i++)
+  {
+    if(i%2==0)
+    {
+        console.log(`this number is even ${i}`);
+    }else{
+        console.log(`this number is even ${i}`);
+    }
+  }
+  
