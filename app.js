d683262334932e9defc08b41ef0bf632943ce179
PratikZajam <zajampratik@gmail.com>
2024-10-07 13:59:04 +0530

    added

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const numbers = [
     19, 23, 4, 16, 28, 13, 31, 8, 29, 14, 6, 35, 2, 11, 17, 5, 9, 27, 12, 30,
   ];
 
+  let  primeNumbers=[];
+
   
   for(i=0;i<=numbers.length-1;i++)
   {
@@ -12,11 +14,39 @@ const numbers = [
         {
             counter++;
         }
-
+       
+       
        
     }
     if(counter==2)
         {
-            console.log(`prime numbers are ${i}`);
+           primeNumbers.push(i)
+
         }
+   
   }
+
+  console.log(primeNumbers)
+
+let min=primeNumbers[0];
+let max=-Infinity;
+
+  for(i=0; i<=primeNumbers.length-1;i++)
+  {
+if(min>primeNumbers[i])
+{
+    min=primeNumbers[i];
+}
+
+if(max<primeNumbers[i])
+    {
+        max=primeNumbers[i];
+    }
+
+  }
+
+  console.log(max);
+  
+  console.log(min);
+
+  
